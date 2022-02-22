44d70360f7b4cbc9ad75872bb1c12da62ca55c6e
ratamon-tech <ratamon91@gmail.com>
2022-02-22 22:23:05 +0900

    add pj1 file

---
+++ pj1.py
@@ -0,0 +1,17 @@
+import random
+import sys
+
+first = ("hoge", "fuga", "piyo")
+last = ("abc", "def", "ghi")
+
+while True:
+    firstName = random.choice(first)
+    lastName = random.choice(last)
+
+    print("{} {}".format(firstName, lastName), file=sys.stderr)
+
+    try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
+    if try_again.lower() == "n":
+        break
+
+input("\nPress Enter to exit.")
