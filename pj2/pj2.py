349d6afc13bc1094ea6f80e515848a8c9545507d
ratamon-tech <ratamon91@gmail.com>
2022-03-03 09:26:21 +0900

    make palindromes list

--- pj2/pj2.py
+++ pj2/pj2.py
@@ -1,10 +1,14 @@
 """
-回文見つける
+辞書ファイルにある回文を見つける
 """
 import load_dictionary
 
 word_list = load_dictionary.load('data/6of12.txt')
 pali_list = []
 
-for word in word_list[:5]:
-    print(word)
+for word in word_list:
+    if len(word) > 1 and word == word[::-1]:
+        pali_list.append(word)
+
+print(f'\nNumber of palindromes found = {len(pali_list)}\n')
+print(*pali_list, sep='\n')
