d4b0594f573f06d10144513deeec0217deae97c4
ratamon-tech <ratamon91@gmail.com>
2022-03-05 15:29:48 +0900

    pylintの内容を修正

--- pj2/pj2.py
+++ pj2/pj2.py
@@ -1,8 +1,9 @@
 """
 辞書ファイルにある回文を見つける
 """
-import load_dictionary
 import os
+
+import load_dictionary
 
 dictionary_path = os.path.abspath('data/6of12.txt')
 word_list = load_dictionary.load(dictionary_path)
@@ -13,6 +14,4 @@ for word in word_list:
         pali_list.append(word)
 
 print(f'\nNumber of palindromes found = {len(pali_list)}\n')
-print(*pali_list[:5], sep='\n')
-print(dictionary_path)
-
+print(*pali_list, sep='\n')
